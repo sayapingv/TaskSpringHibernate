f59c5e2727a200964a5ef0188653112884f7da8e
sayapingv <81882184+sayapingv@users.noreply.github.com>
2021-04-10 10:55:13 +0300

    Add files via upload

---
+++ src/main/java/hiber/MainApp.java
@@ -0,0 +1,53 @@
+package hiber;
+
+import hiber.config.AppConfig;
+import hiber.model.Car;
+import hiber.model.User;
+import hiber.service.UserService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class MainApp {
+   public static void main(String[] args) throws SQLException {
+      AnnotationConfigApplicationContext context = 
+            new AnnotationConfigApplicationContext(AppConfig.class);
+
+      UserService userService = context.getBean(UserService.class);
+
+      Car car1 = new Car("Model1", 1);
+      User user1 = new User("User1", "Lastname1", "user1@mail.ru", car1);
+      car1.setUser(user1);
+      userService.add(user1);
+
+      Car car2 = new Car("Model2", 2);
+      User user2 = new User("User2", "Lastname2", "user2@mail.ru", car2);
+      car2.setUser(user2);
+      userService.add(user2);
+
+      Car car3 = new Car("Model3", 3);
+      User user3 = new User("User3", "Lastname3", "user3@mail.ru", car3);
+      car3.setUser(user3);
+      userService.add(user3);
+
+      Car car4 = new Car("Model4", 4);
+      User user4 = new User("User4", "Lastname4", "user4@mail.ru", car4);
+      car4.setUser(user4);
+      userService.add(user4);
+
+      List<User> users = userService.listUsers();
+      for (User user : users) {
+         System.out.println("Id = "+user.getId());
+         System.out.println("First Name = "+user.getFirstName());
+         System.out.println("Last Name = "+user.getLastName());
+         System.out.println("Email = "+user.getEmail());
+         System.out.println("Car = "+user.getCar());
+         System.out.println();
+      }
+
+      System.out.println(userService.getUserByModelAndSerial("Model2", 2));
+
+      context.close();
+   }
+}
---
+++ src/main/java/hiber/model/Car.java
@@ -0,0 +1,69 @@
+package hiber.model;
+
+import org.hibernate.annotations.Cascade;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "cars")
+public class Car {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "model")
+    private String model;
+
+    @Column(name = "series")
+    private int series;
+
+    @OneToOne
+    @JoinColumn(name = "user")
+    private User user;
+
+    public Car() {
+
+    }
+
+    public Car(String model, int series) {
+        this.model = model;
+        this.series = series;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "model='" + model + '\'' +
+                ", series=" + series +
+                '}';
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getSeries() {
+        return series;
+    }
+
+    public void setSeries(int series) {
+        this.series = series;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
